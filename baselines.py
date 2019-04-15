915384e80d0f37463c7e25df43c29f675f1745d9
Mohammad Zamani <maz.zamani@gmail.com>
2019-04-15 18:33:08 +0200

    +baseline models +  pytorch dataloader

---
+++ baselines.py
@@ -0,0 +1,130 @@
+from sklearn import linear_model
+from sklearn.metrics import mean_absolute_error as mae
+from dataset_loader import BikeDataset
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+
+    # Splitting the dataset into training, validation, and test set
+    # The split can change by train_size and validation_size keywords
+    # The default split are train_size=0.7 and validation_size=0.1
+    traing_set = BikeDataset('train')
+    val_set = BikeDataset( 'val')
+    test_set = BikeDataset( 'test')
+
+    #Linear Regression Model
+    reg = linear_model.LinearRegression()
+    reg.fit(traing_set.x_np,traing_set.y_np)
+    print('Various baselines are demonstrated in this code')
+    print('Linear Regression without regularization')
+
+    prediction = reg.predict(val_set.x_np)
+    target = val_set.y_np
+    print('Validation set loss:  {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+
+    prediction = reg.predict(test_set.x_np)
+    target = test_set.y_np
+    print('Test set loss: {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+    print('================================================================')
+
+
+    reg = linear_model.Ridge(alpha=.5)
+    reg.fit(traing_set.x_np,traing_set.y_np)
+    print('Linear Regression with Ridge regularization')
+
+    prediction = reg.predict(val_set.x_np)
+    target = val_set.y_np
+    print('Validation set loss: {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+
+    prediction = reg.predict(test_set.x_np)
+    target = test_set.y_np
+    print('Test set loss: {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+    print('================================================================')
+
+    reg = linear_model.Lasso(alpha=.1)
+    reg.fit(traing_set.x_np,traing_set.y_np)
+    print('Linear Regression with Lasso regularization')
+
+    prediction = reg.predict(val_set.x_np)
+    target = val_set.y_np
+    print('Validation set loss: {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+
+    prediction = reg.predict(test_set.x_np)
+    target = test_set.y_np
+    print('Test set loss: {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+    print('================================================================')
+
+    reg = linear_model.LassoLars(alpha=.1)
+    reg.fit(traing_set.x_np,traing_set.y_np)
+
+    prediction = reg.predict(val_set.x_np)
+    target =  val_set.y_np
+    print('validation set loss with Lasso LARS regularization {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+
+    prediction = reg.predict(test_set.x_np)
+    target = test_set.y_np
+    print('test set loss with Lasso LARS regularization {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+    #print(reg.coef_)
+    print('================================================================')
+
+    print('Using prvious cnt number as prediction for the current cnt')
+
+    prediction = np.stack(traing_set.y_np[1:])
+    target = np.stack(traing_set.y_np[:-1])
+    print('train error: {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+
+    prediction = np.stack(val_set.y_np[1:])
+    target = np.stack(val_set.y_np[:-1])
+    print('val error  : {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+
+    prediction = np.stack(test_set.y_np[1:])
+    target = np.stack(test_set.y_np[:-1])
+    print('test error : {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+    print('================================================================')
+
+    print('Using the change rate from cnt(t-2) to cnt(t-1) to predict the currrent cnt')
+    prediction = np.stack(traing_set.y_np[2:])
+    target = 2 * np.stack(traing_set.y_np[1:-1]) - np.stack(traing_set.y_np[0:-2])
+    print('train error: {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+
+    prediction = np.stack(val_set.y_np[2:])
+    target = 2 * np.stack(val_set.y_np[1:-1])- np.stack(val_set.y_np[0:-2])
+    print('val error  : {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+
+    prediction = np.stack(test_set.y_np[2:])
+    target = 2 * np.stack(test_set.y_np[1:-1]) - np.stack(test_set.y_np[0:-2])
+    print('test error : {:.2f}, std: {:.2f}'
+          .format(mae(prediction, target), np.std(abs(prediction-target))))
+    print('================================================================')
+
+    print('Using the previous day same hour to predict the current cnt')
+    for myset in [traing_set, val_set, test_set]:
+        error_sum = 0
+        mean_inc = 0
+        for i in range(len(myset.x_np)-24):
+            error_sum += abs(myset.y_np[i -24] - myset.y_np[i])
+        print('{} error: {:.2f}'.format(myset.type, error_sum / (len(myset.x_np)-24)))
+
+    #Showing the histogram of change with respect to previous hour
+    #percent_bins = [-1 + i/10 for i in range(31)] # simpler bin file
+    percent_bins = [-1] + list(np.arange(-.75, .45, .05)) + list(np.arange(.5, 2.75, .25))
+    print('Histogram of relative change compared to previous hour of the following bins are shown:')
+    print('Percentage bins: ',['%.2f' % i for i in percent_bins])
+    plt.figure(1)
+    traing_set = BikeDataset('train', seq_len=2, prev_cnt='hour', percet_bins=percent_bins)
+    plt.hist(traing_set.percentage, bins=percent_bins)
+    plt.show()
---
+++ dataset_loader.py
@@ -0,0 +1,223 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import numpy as np
+
+FLOAT =  torch.FloatTensor
+LONG =torch.LongTensor
+
+
+class BikeDataset(Dataset):
+    """
+    Loading the Bike dataset using Pytorch Dataloader
+    """
+
+    def __init__(self, set_type, seq_len=1, prev_cnt='no', reduced_features=False,
+                 percet_bins=None, max_cnt=None, train_size=0.7, validation_size = 0.1, day_num=1, repeated_data_num=0):
+        """
+        :param timeframe: day or hour
+        :param set_type: train, val, test
+        :param seq_len: int and for LSTM models
+        :param prev_cnt: if the cnt of the previous timestep is given as input. value: no (not given),
+         hour (previous hour), week (day_num previous days and the same hour)
+        :param reduced_features: full feature or the reduced ones which selected from L1 regression
+        """
+
+        # loading csv file
+        data_all = pd.read_csv("dataset/hour.csv")
+
+        self.seq_len = seq_len
+        self.prev_cnt = prev_cnt
+        self.repeated_data_num = 0
+        self.reduced_features = reduced_features
+        self.type = set_type
+        self.repeated_data_num = repeated_data_num
+
+        len_dataset = len(data_all)
+
+        # Train, val and test set separation
+        trn_last_idx = int(train_size * len_dataset)
+        val_last_idx = int((train_size + validation_size)* len_dataset)
+
+        # validation and test set may not start from 00 hr due to a random splitting of dataset
+        # we shift the index to start data from 00 hr
+        shift_index = 0
+
+        # Splitting the dadaset
+        if set_type == 'train':
+            data = data_all[0:trn_last_idx]
+            start_idx = 0
+
+        elif set_type == 'val':
+
+            # start the validation from a 00 hr sample
+            start_idx = trn_last_idx
+            for i in range(trn_last_idx + 1, trn_last_idx + 25):
+                start_idx += 1
+                shift_index += 1
+                if data_all['hr'][start_idx] == 0:
+                    break
+            data = data_all[start_idx : val_last_idx]
+
+        elif set_type == 'test':
+
+            # start the test set from a 00 hr sample
+            start_idx = val_last_idx
+            for i in range(val_last_idx + 1, val_last_idx + 25):
+                start_idx += 1
+                shift_index += 1
+                if data_all['hr'][start_idx] == 0:
+                    break
+            data = data_all[start_idx:]
+
+
+        self.x_np = []
+        self.y_np = []
+
+        # preparing data from the start_idx of the dataset
+        for idx in range(start_idx, start_idx + len(data)):
+
+            # removing this index because many hours missing for those dates
+            if (571 <= idx <= 594) or (5618 <= idx <= 5651):
+                continue
+
+            # Some records are missing from the dataset, we repeat the next hour after those gaps
+            if self.prev_cnt == 'day':
+
+                # Checking if there is a missing hour
+                number_of_repeat = 1 + data['hr'][idx] - (idx + self.repeated_data_num) % 24
+
+                # keeping track of repeated samples to your
+                self.repeated_data_num += (number_of_repeat - 1)
+
+                assert number_of_repeat >= 0
+
+            # if we do not include previous day(s) sample as input data then there is no need to repeat in case of missing data
+            else:
+                number_of_repeat = 1
+
+            # Repeating in case of missing hours
+            for _ in range(number_of_repeat):
+
+                # Dividing feature into important one and not important one based on L1 lin regression
+                important_features = [
+                    data['yr'][idx],
+                    data['hr'][idx] / 23,
+                    data['temp'][idx],
+                    data['atemp'][idx],
+                    data['hum'][idx],
+                ]
+                remaing_features = [
+                    data['season'][idx] / 4,
+                    data['mnth'][idx] / 12,
+                    data['holiday'][idx],
+                    data['weekday'][idx] / 6,
+                    data['workingday'][idx],
+                    data['weathersit'][idx] / 4,
+                    data['windspeed'][idx]
+                ]
+
+                # Packing the list of features in a numpy array
+                if self.reduced_features:
+                    self.x_np.append(np.array(important_features))
+                else:
+                    self.x_np.append(np.array(important_features + remaing_features))
+                self.y_np.append(np.array(data['cnt'][idx], dtype='f'))
+
+           # print('idx=',idx,  ' rec_hr=', data['hr'][idx], ' id%24=',idx % 24, 'correct_hr=', round(self.x_np[-1][1]*23))
+
+        self.feature_length = self.x_np[0].size
+
+        # Converting to torch tensor
+        y_raw = [torch.from_numpy(y) for y in self.y_np]
+        x = [torch.from_numpy(x).type(FLOAT) for x in self.x_np]
+
+        # Normalizing the output values based the maximum bike cnt in the training set
+        if max_cnt is None:
+            self.max_cnt = torch.stack(y_raw).max()
+        else:
+            self.max_cnt = max_cnt
+        y = [val / self.max_cnt for val in y_raw]
+
+        # Printing number of the repeated samples, due to missing hours
+        if self.prev_cnt == 'day':
+            if set_type == 'train':
+                print(
+                    "Some hours are missing from the dataset. So, in case of missing sample, we repeat the next sample")
+            print("number of repeated data in total is ", self.repeated_data_num)
+            if set_type == 'test':
+                print("==================================")
+
+        # Calculating the length of the (train/val/test) set
+        # Also, calculating the shift of index for multi time steps
+        if self.prev_cnt == 'hour' or self.prev_cnt == 'no':
+            self.set_len = len(x) - self.seq_len - 1
+            idx_shift = 1
+        elif self.prev_cnt == 'day':
+            self.set_len = len(x) - self.seq_len - 24 * day_num -1
+            idx_shift = 24 * day_num
+
+
+        # This part prepares the input, output pairs in a list of the torch tensor format
+        # Later, it is used in the __getitem__  function to build the bataches.
+
+
+        # Converting to categorical output
+        if percet_bins:
+            if self.prev_cnt == 'hour' or self.prev_cnt == 'no':
+                hour_num = 1
+            elif self.prev_cnt == 'day':
+                hour_num = 24
+
+            # calculating the relative change respect to the previous hour/day
+            rel_inc = (np.array(y[idx_shift:]) - np.array(y[idx_shift - 1:-1])) / np.array(y[idx_shift - 1:-1])
+            self.percentage = rel_inc
+
+            # converting relative change into discrete categories. The discretization is done based on the given bins.
+            # the discrete output is from 1 to number of bins
+            self.bins = np.array(percet_bins)
+            output_discrete = np.digitize(rel_inc, bins=self.bins)
+
+            # clipping the output in case of more than number of bins. Then minus 1 to start the classes numbers from 0
+            output_categorical = np.clip(output_discrete, 1, len(self.bins)-1)-1
+            assert 0 <= np.min(output_categorical) <= len(self.bins)
+
+            # converting the numpy array to torch tensor
+            self.output = torch.from_numpy(output_categorical).type(torch.long)
+
+            # keeping the continuous output to calculate the MAE
+            self.out_continuous = np.array(y[idx_shift:])
+            self.prev_out_continuous = np.array(y[idx_shift - hour_num:-hour_num])
+
+        # Using continuous output
+        else:
+            self.output = [y[idx + idx_shift + self.seq_len] for idx in range(self.set_len)]
+
+
+
+        # Multiple time steps and no additional info
+        if self.prev_cnt == 'no':
+            self.input = [torch.stack(x[idx+1: idx + self.seq_len+1]) for idx in range(self.set_len)]
+
+        # Multiple time steps and the cnt of the previous hours for each time step is added to the input vector
+        elif self.prev_cnt == 'hour':
+            self.input = [torch.cat((torch.stack(x[idx + 1: idx + self.seq_len + 1]),
+                                     torch.stack(y[idx: idx + self.seq_len]).unsqueeze(1)), dim=1)
+                          for idx in range(self.set_len)]
+
+        # Multiple time steps and the cnt of the same hours in the past day_num days of each time steps are added
+        # as the input vector
+        elif self.prev_cnt == 'day':
+            self.input = torch.zeros((self.set_len, seq_len, day_num + self.feature_length))
+            for idx in range(self.set_len):
+                prev_days_same_hr = torch.zeros(self.seq_len, day_num)
+                for seq in range(self.seq_len):
+                    for day in range(day_num):
+                        prev_days_same_hr[seq, day] = y[idx + 24 * (day + 1) + seq]
+                self.input[idx, :, :] = torch.cat((torch.stack(x[idx: idx + self.seq_len]), prev_days_same_hr), dim=1)
+
+    def __len__(self):
+        return self.set_len
+
+    def __getitem__(self, idx):
+        return self.input[idx], self.output[idx]
